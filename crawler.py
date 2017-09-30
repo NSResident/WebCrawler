04cdbe2a787dd38d2564c858cdf62cd897a41575
rywebber <ryanwebber121@gmail.com>
2017-09-30 04:55:40 -0400

    ._.

--- crawler.py
+++ crawler.py
@@ -1,3 +1,23 @@
 import requests
 
+#Read from crawler.conf
+
+#Crawl from url passed as arguement (?)
+
+
+
+#BFS
+#Search entire page for links, return array of links to search
+#Use queue to store links to crawl
+
+def BFS():
+
+#DFS
+#Search entire page for links, return array of links to search
+#Use stack to store links to crawl
+
+def DFS():
+    return
+
 r = requests.post('https://securitee.org/teaching/cse361/')
+
