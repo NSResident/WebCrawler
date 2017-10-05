d8618175915fe81173b8540e8f464c54224a3e2b
rywebber <ryanwebber121@gmail.com>
2017-10-04 22:42:13 -0400

    TODO updated, crawler.py comments added to searchInit

--- crawler.py
+++ crawler.py
@@ -6,19 +6,24 @@
 #Search entire page for links, return array of links to search
 #Use queue to store links to crawl
 
-def BFS():
-    return
 #DFS
 #Search entire page for links, return array of links to search
 #Use stack to store links to crawl
 
-def DFS():
-    return
-
-
 #Create threads that crawl from urls in queue/stack
 #While crawling look for login form page (?)
-def Crawler():
+
+def searchInit():
+    #Makes q/s
+    #Add first url to q/s
+    #Page counter
+    #Make Q for Parser
+    while counter != 0:
+        #Take from q/s and check for domain, if it does not continue
+        #Call search and return page object
+        #Check if object was empty from page error
+        #Put all links into q/s
+        break
     return
 
 #Create threads that parse texts returned from crawlers (?)
