242e5e0c9bf4462a80547cc66680615f7094e813
RYWStony <ryanwebber121@gmail.com>
2017-10-09 17:32:24 -0400

    Requester GET method updated

--- Requester.py
+++ Requester.py
@@ -4,29 +4,35 @@ class Requester:
     host = ""
     user_agent = "CSE361-KappaBot"
 
-    def __init__(self, user_agent, host):
-        self.host = host
+    def __init__(self, user_agent, domain):
+        self.host = domain
         self.user_agent = user_agent
+        
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Handle if connection not made
+        self.sock.connect((self.host, 443))
 
-    def get(url):
+    def get(self, url = '\\'):
         #Parse url for path
         path = ""
-        header = "HEAD" + path + " HTTP/1.1\n"
-                +"Host: " + host + '\n'
-                +"User-Agent: " + user_agent + '\n'
-                +"Accept-Language: en-US,en"+'\n'
-       
-       sock = socket(AF_INET, SOCK_STREAM)
-       # Handle if connection not made
-       sock.connect(host, 443)
-       sock.send(header)
-       #Handle if nothing comes back(?) 
-       response = sock.recv(4096)
+        header = "GET" + path + " HTTP/1.1\n"+"Host: " \
+                + self.host + '\n'+"User-Agent: " \
+                + self.user_agent + '\n' \
+                + "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8 \n" \
+                + "Accept-Language: en-US,en\n" \
+                + "Connection: keep-alive\n"
+        print header
+        self.sock.send(header)
+        #Handle if nothing comes back(?) 
+        response = self.sock.recv(4096)
 
-       # Check error code and handle 404/500 errors
-       # Parse reponse for payload -> html_page
-       
-       return html_page
+        # Check error code and handle 404/500 errors
+        # Parse reponse for payload -> html_page
+
+        return response 
 
     def post(url):
         return
+
+r =  Requester("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0","superdry.com")
+print r.get()
