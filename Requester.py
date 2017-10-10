55a1609973b7c3cbcb34e4c2fc9c80d610d3783a
RYWStony <ryanwebber121@gmail.com>
2017-10-10 00:26:11 -0400

    Requesting to http works but need loop to get the entire page Updated TODO

--- Requester.py
+++ Requester.py
@@ -4,35 +4,36 @@ class Requester:
     host = ""
     user_agent = "CSE361-KappaBot"
 
-    def __init__(self, user_agent, domain):
+    def __init__(self, domain):
         self.host = domain
-        self.user_agent = user_agent
-        
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Handle if connection not made
-        self.sock.connect((self.host, 443))
+        self.sock.connect((domain, 80))
 
-    def get(self, url = '\\'):
+    def __del__(self):
+        self.sock.close()
+        
+    def get(self, url = '/'):
         #Parse url for path
-        path = ""
-        header = "GET" + path + " HTTP/1.1\n"+"Host: " \
-                + self.host + '\n'+"User-Agent: " \
-                + self.user_agent + '\n' \
-                + "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8 \n" \
-                + "Accept-Language: en-US,en\n" \
-                + "Connection: keep-alive\n"
+        header = ("GET {} HTTP/1.1\r\n" 
+                    "Host: {}\r\n"
+                    "User-Agent:{}\r\n" 
+                    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n" 
+                    "Accept-Language: en-US,en\r\n" 
+                    "Connection: Keep-Alive\r\n\r\n").format(url,self.host,self.user_agent)
+
         print header
         self.sock.send(header)
-        #Handle if nothing comes back(?) 
-        response = self.sock.recv(4096)
+        #Create loop to keep receiving messages until a timeout where no more messages are read
+        response = self.sock.recv(8192)
 
         # Check error code and handle 404/500 errors
         # Parse reponse for payload -> html_page
 
         return response 
 
-    def post(url):
+    def post(self, url, username, password):
         return
 
-r =  Requester("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0","superdry.com")
+r =  Requester("nexon.net")
 print r.get()
