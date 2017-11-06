f6b47aac224f5b7689269942f0728be32ae70dd3
eabdul1127 <eabdul1127@gmail.com>
2017-11-06 01:46:11 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,15 +1,11 @@
 from Tkinter import *
 
-
 root = Tk()
 
 def printdepth():
     BF_text = Text(root, height = 7, width = 30)
     BF_text.grid(row = 7, columnspan = 2)
     BF_text.insert(END, "Some \ntext \nlines \nhello \nworld")
-
-
-
 
 url = Label(root, text = "URL")
 max_depth = Label(root,text="Max Depth")
@@ -29,11 +25,10 @@ robots_txt_button = IntVar()
 Checkbutton(root, text = "Robots.txt", variable = robots_txt_button).grid(row = 3, column = 1, sticky = W)
 dfs_button = IntVar()
 Checkbutton(root, text = "DFS", variable = dfs_button).grid(row = 4, sticky = W)
-bfs_button = IntVar
+bfs_button = IntVar()
 Checkbutton(root, text = "BFS",variable = bfs_button).grid(row = 4, column = 1, sticky = W)
 crawl_button = Button(root, text = "Crawl", command = printdepth)
 crawl_button.grid(row = 5,columnspan = 2, sticky = N+S+E+W)
 BF_button = Button(root, text = "BruteForce", command = printdepth)
 BF_button.grid(row = 6, columnspan = 2, sticky = N+S+E+W)
-#BF_text.grid(row = 6, rowspan = 7, columnspan = 2)
 root.mainloop()
